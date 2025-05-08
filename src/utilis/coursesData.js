a467990fc56412092c35c7fb1df16d5770fcc673
Muskan Jain <61349341+muskanjain318@users.noreply.github.com>
2025-05-08 21:01:17 +0530

    Add files via upload

---
+++ src/utilis/coursesData.js
@@ -0,0 +1,16 @@
+// utils/coursesData.js
+export const coursesData = {
+    UG: {
+      BBA: ['Finance', 'Marketing', 'HR'],
+      BCA: ['Software Engineering', 'AI', 'Data Science'],
+    },
+    PG: {
+      MBA: ['Finance', 'Marketing', 'Operations'],
+      MCA: ['AI', 'Cybersecurity', 'Software Development'],
+    },
+    PhD: {
+      ComputerScience: ['AI', 'Machine Learning'],
+      Economics: ['Macro Economics', 'Micro Economics'],
+    },
+  };
+  
